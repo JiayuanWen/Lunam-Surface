d5e06936f8148a962b862680d07fa49b7afb6505
WeldonWen <jbw5872@psu.edu>
2022-05-02 14:26:11 -0400

    Separated instruction header to save space

--- headers/instructions/instruction.h
+++ headers/instructions/instruction.h
@@ -3,26 +3,6 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
-#include <string>
-#include <iostream>
-#include <limits>
-
-void displayInstruction() {
-    //Clear terminal screen for new frame
-        //"\033[2J:" clears terminal from top to bottom
-        //"\033[1;1H" place cursor back to top right corner, or the begining of terminal.
-    std::cout << "\033[2J\033[1;1H";
-
-    std::cout << "<Instruction> \n \n \n";
-
-    std::cout   << "You are a lunar rover (Marked as 'T'). Your goal is to reach the base (Marked as '!'). \n \n"
-                << "The moon terrian is rough, you can only drive on smooth surfaces ('~'). \n"
-                << "You cannot drive on rough surfaces ('X'). \n \n \n \n";
-    
-    std::cout << "(Press 'Enter' to go back to main menu)";
-    std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-
-    return;
-}
+void displayInstruction();
 
 #endif
