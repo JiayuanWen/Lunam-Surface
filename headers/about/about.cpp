c951a0492ee90e444ef665ba73ae069757e8a9c0
JiayuanWen <jbw5872@psu.edu>
2022-05-14 16:59:39 -0400

    Updated About section

--- headers/about/about.cpp
+++ headers/about/about.cpp
@@ -10,13 +10,15 @@ void displayAbout() {
         //"\033[1;1H" place cursor back to top right corner, or the begining of terminal.
     std::cout << "\033[2J\033[1;1H";
 
-    std::cout << "<About> \n \n \n";
+    std::cout << "<About> \n \n";
 
-    std::cout   << "Designed, coded by Jiayuan Wen \n \n \n"
+    std::cout   << "Lunam Surface \n \n \n"
+                << "Designed, coded by Jiayuan Wen \n \n \n"
                 << "Version: 0.2.0 \n"
                 << "GitHub: https://github.com/JiayuanWen/Lunam-Surface\n \n \n \n";
     
     std::cout << "(Press 'Enter' to go back to main menu)";
+
     std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
 
     return;
